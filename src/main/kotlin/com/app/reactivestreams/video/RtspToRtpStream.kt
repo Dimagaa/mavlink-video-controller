069a0479923b91c6fea3bf9d220b43b19939bf81
Dmytro Martyshchuk <dmartyshchuk@casgate.io>
2026-04-11 01:42:33 +0300

    add caps filter for rtsp stream

--- src/main/kotlin/com/app/reactivestreams/video/RtspToRtpStream.kt
+++ src/main/kotlin/com/app/reactivestreams/video/RtspToRtpStream.kt
@@ -1,5 +1,6 @@
 package com.app.reactivestreams.video
 
+import org.freedesktop.gstreamer.Caps
 import org.freedesktop.gstreamer.Element
 import org.freedesktop.gstreamer.ElementFactory
 import org.freedesktop.gstreamer.Pipeline
@@ -15,32 +16,44 @@ class RtspToRtpStream {
         src.set("location", "rtsp://admin:Admin1234@192.168.1.124:554/cam/realmonitor?channel=1&subtype=0")
         src.set("latency", 0)
 
+        val capsFilter = ElementFactory.make("capsfilter", "caps")
+            ?: error("Failed to create capsfilter")
+
+        val caps = Caps.fromString("application/x-rtp,media=video,encoding-name=H265")
+        capsFilter.set("caps", caps)
+
         val depay = ElementFactory.make("rtph265depay", "depay")
             ?: error("Failed to create depay")
 
+        val parse = ElementFactory.make("h265parse", "parse")
+            ?: error("Failed to create h265parse")
+
         val pay = ElementFactory.make("rtph265pay", "pay")
             ?: error("Failed to create pay")
+
+        pay.set("config-interval", 1)
+        pay.set("pt", 96)
 
         val sink = ElementFactory.make("udpsink", "sink")
             ?: error("Failed to create udpsink")
 
         sink.set("host", "10.222.218.55")
         sink.set("port", 5001)
-
         sink.set("sync", false)
         sink.set("async", false)
 
-        pipeline.addMany(src, depay, pay, sink)
+        pipeline.addMany(src, capsFilter, depay, parse, pay, sink)
 
-        src.connect(Element.PAD_ADDED { element, pad ->
-            val sinkPad = depay.getStaticPad("sink")
+        src.connect(Element.PAD_ADDED { _, pad ->
+            val sinkPad = capsFilter.getStaticPad("sink")
 
             if (!sinkPad.isLinked) {
-                pad.link(sinkPad)
+                val result = pad.link(sinkPad)
+                println("Link result: $result")
             }
         })
 
-        if (!Element.linkMany(depay, pay, sink)) {
+        if (!Element.linkMany(capsFilter, depay, parse, pay, sink)) {
             error("Failed to link depay → pay → sink")
         }
 
